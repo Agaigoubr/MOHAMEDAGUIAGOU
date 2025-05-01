3f34f3e6768b5583003c0efc430f4c3a71563871
Agaigoubr <agaigoubrhim@gmail.com>
2025-05-01 05:29:03 +0100

    update

---
+++ src/components/Description/index.jsx
@@ -0,0 +1,33 @@
+import styles from './style.module.scss';
+import { useInView, motion } from 'framer-motion';
+import { useRef } from 'react';
+import { slideUp, opacity } from './animation';
+import Rounded from '../../common/RoundedButton';
+export default function index() {
+
+    const phrase = "MOHAMED AGUIAGOU Photography Videographyl like photographer Edit photos and videos 📷Only with the phone and I want to have a professional camera dima Morocco 🇲🇦♓.";
+    const description = useRef(null);
+    const isInView = useInView(description)
+    return (
+        <div ref={description} className={styles.description}>
+            <div className={styles.body}>
+                <p>
+                {
+                    phrase.split(" ").map( (word, index) => {
+                        return <span key={index} className={styles.mask}><motion.span variants={slideUp} custom={index} animate={isInView ? "open" : "closed"} key={index}>{word}</motion.span></span>
+                    })
+                }
+                </p>
+                <motion.p variants={opacity} animate={isInView ? "open" : "closed"}>📸 Mohamed Aguiagou
+Photographie & Vidéographie
+🎥 Tout au téléphone | Pro cam bientôt
+🇲🇦 From Morocco amazir  | ♓ Passion & visi.</motion.p>
+                <div data-scroll data-scroll-speed={0.1}>
+                    <Rounded className={styles.button}>
+                        <p>About me</p>
+                    </Rounded>
+                </div>
+            </div>
+        </div>
+    )
+}
---
+++ src/components/Header/nav/Footer/index.jsx
@@ -0,0 +1,14 @@
+import styles from './style.module.scss';
+
+export default function index() {
+  return (
+    <div className={styles.footer}>
+        <a>Facebook</a>
+
+        <a href="https://www.instagram.com/agaygoumohamed/">Instagram</a>
+     
+        <a>Dribble</a>
+        <a>LinkedIn</a>
+    </div>
+  )
+}
